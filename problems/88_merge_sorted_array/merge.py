bc55e69d3c637d012e11c47e43c6c1841f4aecd6
donaldKelly17 <donaldkelly17@gmail.com>
2025-04-29 20:20:55 +0100

    added Docstring with params and returns

--- problems/88_merge_sorted_array/merge.py
+++ problems/88_merge_sorted_array/merge.py
@@ -4,7 +4,11 @@ from typing import List
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
-        Do not return anything, modify nums1 in-place instead.
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
         """
-        nums1[m : m + n] = nums2[:]
+        nums1[m:] = nums2[:]
         nums1.sort()
