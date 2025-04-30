4779cddb9187d9723e98b99dd182c7848c719ea3
donaldKelly17 <donaldkelly17@gmail.com>
2025-04-30 20:07:43 +0100

    169.  Improved solution based on tutorial. ref in code

--- problems/169_majority_element copy/majority_element.py
+++ problems/169_majority_element copy/majority_element.py
@@ -1,22 +1,25 @@
 from typing import List
 
+"""
+Version 2:  An improved version based on this tutorial:
+https://youtu.be/c1B3LZQtZ_s?si=NRph-XvDQuKxXNt3
+
+"""
+
 
 class Solution:
     def majority_element(self, nums: List[int]) -> int:
-        n = len(nums)
-        freq = {}
-        max_cnt = 0
+        cnt = 0
         res = -1
 
-        # Create hash table from the list nums
-        for i in range(n):
-            freq[nums[i]] = freq.get(nums[i], 0) + 1
+        for num in nums:
+            if cnt == 0:
+                res = num
 
-        # find the majority element from hash table
-        for value, cnt in freq.items():
-            if max_cnt < cnt or (cnt == max_cnt and value > res):
-                res = value
-                max_cnt = cnt
+            if res == num:
+                cnt += 1
+            else:
+                cnt -= 1
 
         return res
 
