d1f955746eb44504a50876fbc80b5e304c143e7d
donaldKelly17 <donaldkelly17@gmail.com>
2025-04-30 14:37:27 +0100

    26. Improved solution using two pointers

--- problems/26_remove_duplicates/remove_duplicates.py
+++ problems/26_remove_duplicates/remove_duplicates.py
@@ -1,8 +1,25 @@
 from typing import List
 
+"""
+Version 2: Improved solution using two pointers.
+
+Pointer 1: i
+Pointer 2: k
+
+Complexity
+Time: O(n)
+Space: O(1)
+"""
+
 
 class Solution:
     def remove_duplicates(self, nums: List[int]) -> int:
-        nums[:] = list(dict.fromkeys(nums))
+        k = 1  # pointer 2
+        n = len(nums)
 
-        return len(nums)
+        for i in range(1, n):
+            if nums[i] != nums[i - 1]:
+                nums[k] = nums[i]
+                k += 1
+
+        return k
