12165845465c303c33f47bb99e7ee1a11274bed2
donaldKelly17 <donaldkelly17@gmail.com>
2025-05-27 10:55:51 +0100

    Improved solution. Reduction in time and space

--- problems/125_valid_palindrome/is_palindrome.py
+++ problems/125_valid_palindrome/is_palindrome.py
@@ -1,19 +1,39 @@
-import re
+"""
+An improved solution based on:
+    - https://youtu.be/Nxzstmv_U0U?si=Xjh4KN74WhSImG_J
+    - https://youtu.be/jJXJ16kPFWg?si=dAuH3qNHTyqOyiiS
+
+First solution:
+    Runtime = 9ms  (O(N))
+    Memory = 19.3 MB  (O(1))
+
+Improved solution:
+    Runtime = 7ms  (O(N))
+    Memory = 18.03 MB  (O(1))
+"""
 
 
 class Solution:
     def is_palindrome(self, s: str) -> bool:
-        s = re.sub(r"[^a-zA-Z0-9]", "", s)
-        s = s.lower()
+        n = len(s)
         x = 0
-        y = len(s)
+        y = n - 1
 
-        if y <= 1:
+        if n <= 1:
             return True
 
-        while x <= y:
-            if s[x] != s[y - 1]:
+        while x < y:
+            if not s[x].isalnum():
+                x += 1
+                continue
+
+            if not s[y].isalnum():
+                y -= 1
+                continue
+
+            if s[x].lower() != s[y].lower():
                 return False
+
             x += 1
             y -= 1
 
